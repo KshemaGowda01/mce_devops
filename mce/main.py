642045d4febc8d454ddf9f33b027afbfa992b40b
KshemaGowda01 <kshemagowda505@gmail.com>
2024-07-02 15:49:33 +0530

    main

--- mce/main.py
+++ mce/main.py
@@ -6,8 +6,8 @@ from division import divide
 def main():
     print("Calculation")
     
-    a = 13
-    b = 28
+    a = 10
+    b = 9
     
     print(f"Addition of {a} and {b}: {add(a, b)}")
     print(f"Subtraction of {a} and {b}: {subtract(a, b)}")
